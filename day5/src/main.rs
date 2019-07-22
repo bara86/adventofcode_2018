cc26ff3a7b1aad3def10e614336385f1cc3bc55d
Bara <bara@develer.com>
2019-07-22 19:26:33 +0200

    Add day5 part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -10,9 +10,22 @@ fn main() -> std::io::Result<()> {
 
     reader.read_line(&mut polymers)?;
 
+    let best = (b'a'..b'z').map(|c_lower| {
+        let c_lower = c_lower as char;
+        let c_upper = c_lower.to_ascii_uppercase();
+
+        reaction(polymers.chars().filter(|c| *c != c_lower && *c != c_upper)).len()
+    }).min();
+
+    println!("{:?}", best);
+
+    Ok(())
+}
+
+fn reaction(polymers: impl Iterator<Item=char>) -> String {
     let mut new_string = String::new();
 
-    for c in polymers.chars() {
+    for c in polymers {
         if let Some(last_char) = new_string.chars().last() {
             if (last_char as i32 - c as i32).abs() == 32 {
                 new_string.pop();
@@ -23,7 +36,5 @@ fn main() -> std::io::Result<()> {
         new_string.push(c);
     }
 
-    println!("{}", new_string.len());
-
-    Ok(())
+    new_string
 }
